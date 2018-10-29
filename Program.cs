3dd47cbb31cc2930db132938827131df5ef54210
l-kimleng <l.kimleng.surface@hotmail.com>
2018-10-29 12:08:30 -0400

    format display

--- Program.cs
+++ Program.cs
@@ -13,17 +13,43 @@ namespace FindPet
             var s = new Solver();
             s.Mock();
 
-            for(int h = 0; h < s.Houses.Length; h++)
+            var formatedData = TransformResult(s.Houses);
+            Console.WriteLine("House\t\t1\t\t2\t\t3\t\t4\t\t5");
+            foreach(var data in formatedData)
             {
-                Console.WriteLine($"House:\t{h + 1}" +
-                    $"\tColor:{s.Houses[h].Color}" + 
-                    $"\tNationality:{s.Houses[h].Nationality}"+
-                    $"\tDrink:{s.Houses[h].Drink}" +
-                    $"\tSmoke:{s.Houses[h].Smoke}" +
-                    $"\t\tPet: {s.Houses[h].Pet}");
+                var str = (data.Key.Length >= 9) ? data.Key.Substring(0, 4) + "..." : data.Key;
+                foreach(var ele in data.Value)
+                {
+                    var value = ele;
+                    if (ele.Length >= 9)
+                    {
+                        value = value.Substring(0, 4) + "...";
+                    }
+                    
+                    str += "\t\t" + value;
+                }
+                Console.WriteLine(str);                    
             }
-
             Console.ReadLine();
+        }
+
+        private static Dictionary<string, List<string>> TransformResult(House[] houses)       
+        {
+            var result = new Dictionary<string, List<string>>();
+            for (int index = 0; index < houses.Length; index++)
+            {
+                var obj = houses[index];
+                foreach (var prop in obj.GetType().GetProperties())
+                {
+                    var key = prop.Name;
+                    if (!result.ContainsKey(key))
+                    {
+                        result[key] = new List<string>();
+                    }
+                    result[key].Add((string)prop.GetValue(obj, null));
+                }
+            }
+            return result;
         }
     }
 }
